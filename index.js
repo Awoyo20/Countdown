c451f047d0e3bbdc374e92f2d36376b94dd86097
Awoyo@20 <awoyooluwadamola@gmail.com>
2022-05-16 10:49:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+function countdown(){
+    var now = new Date();
+    var eventDate = new Date(2022, 05, 16);
+
+    var currentTime = now.getTime();
+    var eventTime = eventDate.getTime();
+
+    var remTime = eventTime - currentTime;
+
+    var s = Math.floor(remTime / 1000);
+    var m = Math.floor (s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    h = (m < 10) ? "0" + m : m;
+    h = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").textContent = d;
+    document.getElementById("days").innerText = d;
+
+    document.getElementById("hours").textContent = h;
+    document.getElementById("minutes").textContent = m;
+    document.getElementById("seconds").textContent = s;
+
+    setTimeout(countdown, 1000)
+}
+countdown();
+
+alert("What is your Name")
